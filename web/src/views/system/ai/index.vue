b01e23c697dd3f0224ca75d4d96cc18e2e5032c4
dcq <dcq0516@gmail.com>
2026-01-22 23:57:15 +0800

    update ai-view

--- web/src/views/system/ai/index.vue
+++ web/src/views/system/ai/index.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const model = reactive({
+    name: '',
+    apiKey: ''
+  })
+</script>
 
 <template>
-  <div>Ai管理</div>
+  <div class="art-full-height">
+    <ElCard class="art-table-card" shadow="never">
+      <ElForm :model="model" label-width="86px" label-position="top">
+        <ElFormItem label="模型名称" prop="name">
+          <ElInput v-model="model.name" />
+        </ElFormItem>
+        <ElFormItem label="ApiKey" prop="apiKey">
+          <ElInput type="password" v-model="model.apiKey" show-password />
+        </ElFormItem>
+      </ElForm>
+    </ElCard>
+  </div>
 </template>
